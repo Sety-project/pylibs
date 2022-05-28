3b84c63120ff96a813c11a4cf8c2620e2fa7c82c
vic <victor.houlet@gmail.com>
2022-05-28 16:10:23 +0000

    adding entrypoint run script for dockerimage and main entrypoint

---
+++ pfoptimizer/main.py
@@ -0,0 +1,6 @@
+#!/bin/python3
+import sys
+from pfoptimizer.portoflio_optimizer import main
+
+if __name__ == "__main__":
+    main(sys.argv)
---
+++ pfoptimizer/run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+python3 main.py $EXCHANGE_NAME $RUN_TYPE
