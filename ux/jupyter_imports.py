143de1742a21398847a0822d0088be1cc7ecb0f0
david <davidrelkin@gmail.com>
2022-06-26 00:54:56 +0000

    clean up notebooks

---
+++ ux/jupyter_imports.py
@@ -0,0 +1,15 @@
+import os,sys
+sys.path.append('../')
+from dateutil import parser
+from datetime import *
+import numpy as np
+import pandas as pd
+
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import cufflinks as cf
+cf.go_offline()
+cf.set_config_file(offline=False, world_readable=True)
+
+import nest_asyncio
+nest_asyncio.apply()
