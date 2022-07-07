ba84c84b33f1336efe8aa61aac74a1098569a2bc
david <davidrelkin@gmail.com>
2022-07-07 13:36:19 +0000

    minor fix

--- pfoptimizer/run.sh
+++ pfoptimizer/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 main.py $ORDER config=$CONFIG EXCHANGE=$EXCHANGE subaccount=$SUBACCOUNT
+python3 main.py $ORDER $EXCHANGE config=$CONFIG subaccount=$SUBACCOUNT
--- tradeexecutor/run.sh
+++ tradeexecutor/run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 main.py $ORDER exchange=$EXCHANGE subaccount=$SUBACCOUNT
+python3 main.py $ORDER config_dir=$CONFIG exchange=$EXCHANGE subaccount=$SUBACCOUNT
