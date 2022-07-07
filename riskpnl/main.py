82a17430823eaf20f18bc247cfa4d47507ac5b46
david <davidrelkin@gmail.com>
2022-07-07 09:37:21 +0000

    API refactor

--- riskpnl/main.py
+++ riskpnl/main.py
@@ -3,4 +3,6 @@ import sys
 from riskpnl.ftx_risk_pnl import main
 
 if __name__ == "__main__":
-    main(sys.argv)
+    args = [arg.split('=')[0] for arg in sys.argv if len(arg.split('=')) == 1]
+    kwargs = dict([arg.split('=') for arg in sys.argv if len(arg.split('=')) == 2])
+    main(*args,**kwargs)
