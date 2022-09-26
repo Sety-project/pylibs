3f392cc5296c6c28079af4a28abc823456caa724
daviidarr <davidrelkin@gmail.com>
2022-09-26 16:40:35 +0000

    major margin bug

--- tradeexecutor/position_manager.py
+++ tradeexecutor/position_manager.py
@@ -151,7 +151,7 @@ class PositionManager(dict):
             return 0
 
         marginal_IM = marginal_IM if abs(marginal_IM) > 1e-9 else np.sign(marginal_IM) * 1e-9
-        if actual_IM + marginal_IM < self.margin.IM_buffer:
+        if actual_IM - marginal_IM < self.margin.IM_buffer:
             trim_factor = np.clip((self.margin.IM_buffer - actual_IM) / marginal_IM,a_min=0,a_max=1)
         else:
             trim_factor = 1.0
