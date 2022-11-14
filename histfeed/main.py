9837c02a5d4e3a823c5755cce447939a36104c83
daviidarr <davidrelkin@gmail.com>
2022-11-14 16:09:45 +0000

    adding binance

--- histfeed/main.py
+++ histfeed/main.py
@@ -2,7 +2,7 @@
 import sys
 from utils.api_utils import extract_args_kwargs
 
-from histfeed.ftx_history import main
+from histfeed.binance_history import main
 
 if __name__ == "__main__":
     args, kwargs = extract_args_kwargs(sys.argv)
