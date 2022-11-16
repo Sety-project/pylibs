b802de900305eb8acb3140127e2112c2fe59069e
daviidarr <davidrelkin@gmail.com>
2022-11-16 18:19:59 +0000

    just a name change

--- tradeexecutor/main.py
+++ tradeexecutor/main.py
@@ -2,7 +2,7 @@
 import sys
 from utils.api_utils import extract_args_kwargs
 
-from tradeexecutor.ftx_ws_execute import main
+from tradeexecutor.execute import main
 
 if __name__ == "__main__":
     args, kwargs = extract_args_kwargs(sys.argv)
