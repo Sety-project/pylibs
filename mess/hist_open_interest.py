fead3e13fadb8d23632bc5f2c4fa1170aa73c28b
daviidarr <davidrelkin@gmail.com>
2022-12-05 11:30:09 +0000

    minor

--- mess/hist_open_interest.py
+++ mess/hist_open_interest.py
@@ -2,13 +2,15 @@ import asyncio
 import datetime
 from utils.ccxt_utilities import open_exchange
 from utils.async_utils import safe_gather
+import pandas as pd
 
 async def futs():
     exchange = await open_exchange('binanceusdm','')
     markets = await exchange.fetch_markets({'type': 'future'})
-    ftt_futures = [market['symbol'] for market in markets if market['base'] == 'FTT' and market['swap'] == True]
+    ftt_futures = [market['symbol'] for market in markets if market['swap'] == True]
     list_oi = await safe_gather([exchange.fetch_open_interest_history(market, limit=500) for market in ftt_futures])
     return dict(zip(ftt_futures, [{datetime.datetime.fromtimestamp(y['timestamp']/1000) : float(y['info']['sumOpenInterestValue']) for y in x} for x in list_oi]))
 
 d = asyncio.run(futs())
+pd.concat([pd.Series(name=key,data=data).sort_index() for key,data in d.items()])
 print(d)
