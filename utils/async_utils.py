9a4650190beb2db695ade5a6322c1ad5f7bc4b74
daviidarr <nononymous@gmail.com>
2024-06-02 16:23:39 +0200

    gather_limit = 3

--- utils/async_utils.py
+++ utils/async_utils.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python3
 import platform
-import asyncio, threading
+import asyncio
 import functools
 
-if platform.system()=='Windows':
+if platform.system() == 'Windows':
     asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
 
-safe_gather_limit = 10
+safe_gather_limit = 3
+
 
 def async_wrap(f):
     '''
@@ -20,16 +21,11 @@ def async_wrap(f):
         return await loop.run_in_executor(executor, p)
     return run
 
-async def safe_gather(tasks,n=safe_gather_limit,semaphore=None,return_exceptions=False):
-    semaphore = semaphore if semaphore else asyncio.Semaphore(n)
+
+async def safe_gather(tasks, n=safe_gather_limit, semaphore=None, return_exceptions=False):
+    semaphore = semaphore or asyncio.Semaphore(n)
 
     async def sem_task(task):
         async with semaphore:
             return await task
-    return await asyncio.gather(*(sem_task(task) for task in tasks),return_exceptions=return_exceptions)
-
-
-class CustomRLock(threading._PyRLock):
-    @property
-    def count(self):
-        return self._count
+    return await asyncio.gather(*(sem_task(task) for task in tasks), return_exceptions=return_exceptions)
